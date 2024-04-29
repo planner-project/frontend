d337b3e5ae7b24c8556543645d4f3cb41476b78a
lsh <jetaime12121@gmail.com>
2024-04-29 21:38:54 +0900

    Add 회원가입, 로그인 layout (#3)

---
+++ src/components/Button.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+const StyledBtn = styled.button`
+  min-width: 100px;
+  height: 50px;
+  width: ${(props) => (props.fullWidth ? "100%" : "auto")};
+  border: none;
+  padding: 0 10px;
+  border-radius: 10px;
+  background-color: #d0d5dd;
+  font-weight: bold;
+  font-size: 1.2rem;
+  cursor: pointer;
+`;
+
+const BlueBtn = styled(StyledBtn)`
+  background-color: #1570ef;
+  color: #fff;
+  font-weight: normal;
+  font-size: 1rem;
+`;
+
+export { StyledBtn, BlueBtn };
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,50 @@
+import { StyledBtn, BlueBtn } from "../components/Button.jsx";
+import {
+  StyledFormLayout,
+  StyledForm,
+  StyledTitle,
+  StyledCaption,
+  StyledLink,
+  StyledInput,
+  StyledFlexWrap,
+  StyledParagraph,
+  ImageWrap,
+} from "../components/Form.jsx";
+
+const Signup = () => {
+  return (
+    <StyledFormLayout>
+      <StyledForm>
+        <StyledTitle>회원가입</StyledTitle>
+        <StyledCaption>
+          이미 아이디가 있으신가요?{" "}
+          <StyledLink to="/login">로그인 하러가기</StyledLink>
+        </StyledCaption>
+        <StyledFlexWrap>
+          <StyledInput placeholder="Email" />
+          <BlueBtn>Email 인증</BlueBtn>
+        </StyledFlexWrap>
+        <StyledInput placeholder="닉네임" mb />
+        <StyledInput placeholder="비밀번호" mb />
+        <StyledBtn fullWidth>가입하기</StyledBtn>
+        <StyledParagraph>소셜 로그인</StyledParagraph>
+        <ImageWrap>
+          <img
+            src="images/google-logo.png"
+            alt="구글 로그인"
+            role="button"
+            aria-label="구글로 로그인 하기"
+          />
+          <img
+            src="images/kakao-logo.png"
+            alt="구글 로그인"
+            role="button"
+            aria-label="구글로 로그인 하기"
+          />
+        </ImageWrap>
+      </StyledForm>
+    </StyledFormLayout>
+  );
+};
+
+export default Signup;
