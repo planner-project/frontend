f333b50732fa585a551b664e8fae8db6a912ea01
soominnnn <105346651+soominnnn@users.noreply.github.com>
2024-06-03 01:50:43 +0900

    Add: 실시간 채팅 (#51)

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,6 +1,11 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import Modal from "react-modal";
 import styled, { css } from 'styled-components';
+import SockJS from 'sockjs-client';
+import { Client } from '@stomp/stompjs';
+import { StyledInput } from './Form';
+import useUserStore from '../store';
+import { Typo } from './Typo';
 
 const ChatButton = styled.div`
   width: 85px;
@@ -11,7 +16,7 @@ const ChatButton = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-`
+`;
 
 const ChatContainer = styled.div`
   width: 426px;
@@ -21,6 +26,14 @@ const ChatContainer = styled.div`
   margin-left: 55vw;
   overflow-y: hidden;
 `;
+
+const MessageContainer = styled.div`
+  width: 426px;
+  display: flex;
+  flex-direction: column;
+  height: 500px;
+  overflow-y: scroll;
+`
 
 const customStyles = {
   content: {
@@ -39,43 +52,126 @@ const customStyles = {
     zIndex: "10",
   },
 };
+
 const MessageWrapper = styled.div`
   display: flex;
-  ${(props =>
-    props.$justify &&
+  flex-direction: column;
+  ${(props) =>
+    props.$align &&
     css`
-      justify-content: ${props.$justify};
-    `
-  )}
-`
+      align-items: ${props.$align};
+    `}
+`;
+
 const UserMessageContainer = styled.div`
   width: 151px;
   height: auto;
   padding: 10px;
   border-radius: 5px;
-  ${(props =>
+  ${(props) =>
     props.$backgroundColor &&
     css`
       background-color: ${props.$backgroundColor};
-    `
-  )}
+    `}
 `;
 
 const Message = styled.p`
   font-size: 9px;
 `;
 
-
 const Chat = () => {
   const [modalOpen, setModalOpen] = useState(false);
+  const [connected, setConnected] = useState(false);
+  const [messageContent, setMessageContent] = useState('');
+  const [messages, setMessage] = useState([]);
+  const client = useRef(null);
+  const { user } = useUserStore();
+
+  useEffect(() => {
+    const token = sessionStorage.getItem("Authorization").split(" ")[1];
+    const socketURL = `http://localhost:8080/ws?token=${token}`;
+
+    client.current = new Client({
+      webSocketFactory: () => new SockJS(socketURL),
+      debug: (str) => console.log(str),
+      reconnectDelay: 5000,
+      heartbeatIncoming: 4000,
+      heartbeatOutgoing: 4000,
+    });
+
+    client.current.onConnect = (frame) => {
+      console.log('Connected');
+      setConnected(true);
+      subscribe();
+    };
+
+    client.current.onDisconnect = (frame) => {
+      setConnected(false);
+      console.log('Disconnected');
+    };
+
+    client.current.onStompError = (frame) => {
+      console.error('Broker reported error: ' + frame.headers['message']);
+      console.error('Additional details: ' + frame.body);
+    };
+
+    client.current.activate();
+
+    return () => {
+      if (client.current) {
+        client.current.deactivate();
+      }
+    };
+  }, []);
+
   const showModal = () => {
     setModalOpen(true);
-  }
+  };
+
   const closeModal = () => {
     setModalOpen(false);
-  }
+  };
 
-  return(
+  const publish = () => {
+    try {
+      const message = {
+        userId: user.userId,
+        nickname: user.nickname,
+        profileImgUrl: user.profileImgUrl,
+        message: messageContent,
+      };
+      client.current.publish({
+        destination: "/pub/planner/1/chat/send",
+        body: JSON.stringify(message),
+      });
+    } catch (error) {
+      console.log(user);
+      console.log(error);
+    }
+  };
+
+  const subscribe = () => {
+    const subscriptionUrl = "/sub/planner/1";
+
+    client.current.subscribe(subscriptionUrl, (response) => {
+      const messageObject = JSON.parse(response.body);
+      if (messageObject.type === "chat") {
+        const object = {
+          ...messageObject,
+          isMe: messageObject.message.userId === user.userId,
+        };
+        setMessage((prevMessages) => [...prevMessages, object]);
+      }
+    });
+  };
+
+  const activeEnter = (e) => {
+    if (e.key === "Enter") {
+      publish();
+    }
+  };
+
+  return (
     <ChatContainer>
       <ChatButton onClick={showModal}>
         <img src="../../message.png" alt="메시지 로고" />
@@ -85,15 +181,24 @@ const Chat = () => {
         style={customStyles}
         ariaHideApp={true}
         onRequestClose={closeModal}
-        >
-        <MessageWrapper>
-          <UserMessageContainer>
-            <Message>gd</Message>
-          </UserMessageContainer>
-        </MessageWrapper>
+      >
+        <MessageContainer>
+        {messages.map((user, index) => (
+          <MessageWrapper key={index} $align={user.isMe ? "end" : "start"}>
+            <Typo>{user.message.nickname}</Typo>
+            <UserMessageContainer $backgroundColor={user.isMe ? "blue" : "gray"}>
+              <Message>{user.message.message}</Message>
+            </UserMessageContainer>
+          </MessageWrapper>
+        ))}
+        </MessageContainer>
+        <StyledInput
+          onChange={(e) => setMessageContent(e.target.value)}
+          onKeyDown={(e) => activeEnter(e)}
+        />
       </Modal>
     </ChatContainer>
   );
-}
+};
 
 export default Chat;
