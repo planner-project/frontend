b38e7f2e160bec20837ba34269c29fb3e169ff91
soominnnn <105346651+soominnnn@users.noreply.github.com>
2024-06-02 15:06:03 +0900

    Fix: onClick 이벤트 수정

--- src/components/SearchListItem.jsx
+++ src/components/SearchListItem.jsx
@@ -1,9 +1,9 @@
 import { ListItem } from "./SearchInput";
 import { Typo } from "./Typo";
 
-const SearchListItem = ({nickname, userTag, email}) => {
+const SearchListItem = ({nickname, userTag, email, onCLick}) => {
   return (
-    <ListItem>
+    <ListItem onClick={onCLick}>
       <Typo $size="0.9rem">{nickname} #{userTag}</Typo>
       <Typo $size="0.8rem" $color="#6a6a6a" $margin="5px 0 0">
         {email}
